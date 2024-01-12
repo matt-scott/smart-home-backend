bc703ced4a80eb04507524890244dca73d82e1ae
Matt Scott <mattscott251@gmail.com>
2024-01-12 12:45:57 -0600

    all mqtt services working

--- app/mqttjs/index.js
+++ app/mqttjs/index.js
@@ -4,9 +4,14 @@ const mqtt = require('mqtt');
 const host = ('172.17.0.1');
 // Default unencrypted MQTT port. Mosquitto connects to this automatically when run.
 const port = ('1883');
-const clientId = (`mqttjs_handler${Math.random().toString(16).slice(3)}`);
+const clientId = (`mqttjs_handler_${Math.random().toString(16).slice(11)}`);
+// const clientId = (`mqttjs_handler`);
 
 const connectUrl = (`mqtt://${host}:${port}`);
+
+const getRoom = (topic) => {
+  return topic.split('/')[1];
+};
 
 const client = mqtt.connect(connectUrl, {
   clientId,
@@ -17,7 +22,7 @@ const client = mqtt.connect(connectUrl, {
   reconnectPeriod: 1000,
 });
 
-const topic = ('zigbee2mqtt/#');
+const topic = ('zigbee2mqtt/+/temperature');
 
 // Subscribe to topic variable on connection with MQTT URL
 client.on('connect', () => {
@@ -26,15 +31,17 @@ client.on('connect', () => {
   client.subscribe([topic], () => {
     console.log(`Subscribe to topic '${topic}'`);
     // Publish a test message to verify subscription is working
-    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-      if (error) {
-        console.error(error);
-      }
-    });
+    // client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    //   if (error) {
+    //     console.error(error);
+    //   }
+    // });
   });
 });
 
 // Publish any messages received from the topic to the console
 client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString());
+  const room = getRoom(topic);
+  console.log(room);
+  console.log(JSON.parse(payload.toString()));
 });
