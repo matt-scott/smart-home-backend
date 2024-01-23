76c9de649448b669211f3f17460cbab81c4d4f21
Matt Scott <mattscott251@gmail.com>
2024-01-22 20:55:32 -0600

    progress updates

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ const clientId = (`mqttjs_database_handler_${Math.random().toString(16).slice(11
 
 const connectUrl = (`mqtt://${host}:${port}`);
 
-const getRoom = (topic) => {
+const getSensorLocation = (topic) => {
   return topic.split('/')[1];
 };
 
@@ -75,32 +75,56 @@ client.on('connect', () => {
   });
 });
 
-// Function to build db object
-// Turn into class or instance level method?
-// https://sequelize.org/docs/v6/core-concepts/model-basics/
-const dbObjectBuilder = () => {
 
-};
 
 // Publish any messages received from the topic to the console
 client.on('message', async (topic, payload) => {
-  const room = getRoom(topic);
-  console.log(room);
-  console.log(JSON.parse(payload.toString()));
 
-  const objectDeliverable = dbObjectBuilder(topic, payload);
+
+
+  // Build new object to publish to db
+  // Turn into class or instance level method?
+  // https://sequelize.org/docs/v6/core-concepts/model-basics/
 
   // get the current time
-  const time = new Date().getTime();
+  const dataObject = JSON.parse(payload.toString());
+  console.log(dataObject);
+
+  const timestamp = new Date().getTime();
+  const sensorLocation = getSensorLocation(topic);
+  console.log(sensorLocation);
+
 
   try {
-      // insert the record
-      await PageLoads.create({
-          userAgent, time
-      });
 
-      // send response
-      console.log('Message inserted into db!');
+    // Refactor using spread operator. Don't need to write out all dataObject items.
+    // Verify dataObject items align with the below first though
+    const newDataEntryObject = temperature_sensor_data.build({
+      timestamp: timestamp,
+      sensor_location: sensorLocation,
+      measurement_type: "temperature",
+      battery: dataObject.battery,
+      humidity: dataObject.humidity,
+      linkquality: dataObject.linkquality,
+      power_outage_count: dataObject.power_outage_count,
+      pressure: dataObject.pressure,
+      temperature: dataObject.temperature,
+      voltage: dataObject.voltage
+    });
+
+
+    // // Check if tree is in the database
+    // await Tree.findOne({ where: { tree: tree } }); // returns null
+
+    // Insert the newTree instance into the database
+    await newDataEntryObject.save();
+
+    // // Check if tree is in the database
+    // await Tree.findOne({ where: { tree: tree } }); // returns Tree's record
+
+    // send response
+    console.log(newDataEntryObject)
+    console.log('Data inserted into db!');
   } catch (e) {
       console.log('Error inserting data into db', e)
   }
